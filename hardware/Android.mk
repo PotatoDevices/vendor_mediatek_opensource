aac5abc9290fac0b10bd75b7d634ee8cd2f18a4a
Zinadin Zidan <zidan44@pixelexperience.org>
2020-12-31 14:04:15 +0600

    vendor-mtk: hardware: Import MTK power hardware service
    - Based on ALPS Q MP1 V9.122.1
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I51c508e700ddbaebc56dce9b17af63d8d4a82b94

--- hardware/Android.mk
+++ hardware/Android.mk
@@ -2,3 +2,5 @@ MTK_HARD_ROOT_PATH := $(call my-dir)
 
 # MTK Power
 include $(MTK_HARD_ROOT_PATH)/power/service/Power/1.3/Android.mk
+include $(MTK_HARD_ROOT_PATH)/power/service/main/Android.mk
+include $(MTK_HARD_ROOT_PATH)/power/service/MtkPower/1.0/Android.mk
--- mtk-oss.mk
+++ mtk-oss.mk
@@ -6,4 +6,5 @@
 
 # hardware
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-impl-mediatek
+    android.hardware.power@1.3-impl-mediatek \
+    vendor.mediatek.hardware.mtkpower@1.0-service
